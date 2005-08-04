30b79c5357cefb6bd719033325d2ec2d0c625249
lothar <lothar@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-08-04 09:43:20 +0000

    - Deleted support for the old interface

--- branches/concentrator-receiver-work/src/testCollector.c
+++ branches/concentrator-receiver-work/src/testCollector.c
@@ -13,82 +13,22 @@
 #include "common.h"
 
 #define DEFAULT_LISTEN_PORT 4711
-#define USE_OLD_IFACE 0
-#define USE_NEW_IFACE 1
 
 void sigint() {
-	}
+}
 
-void start_collector_using_old_interface(int);
-void start_collector_using_new_interface(int);
 void usage();
 
 int main(int argc, char *argv[]) {
 
         int lport=DEFAULT_LISTEN_PORT;
-	int c;
-	int iface = USE_NEW_IFACE;
+	
+	if (argc > 1) {
+		lport = atoi(argv[1]);
+	}
 
         signal(SIGINT, sigint);
 
-	while ((c = getopt(argc, argv, "p:onh")) != -1) {
-		switch (c) {
-		case 'p':
-			lport = atoi(optarg);
-			break;
-		case 'o':
-			iface = USE_OLD_IFACE;
-			break;
-		case 'n':
-			iface = USE_NEW_IFACE;
-			break;
-		case 'h':
-		default:
-			usage(argv[0]);
-			exit(0);
-		}
-	}
-
-	if (iface == USE_OLD_IFACE) {
-		debug("Starting up using old interface");
-		start_collector_using_old_interface(lport);
-	} else {
-		start_collector_using_new_interface(lport);
-		debug("Starting up using new interface");
-	}
-	return 0;
-	}
-
-
-
-void start_collector_using_old_interface(int lport) {
-	/*
-	initializeIpfixPrinters();
-
-	initializeIpfixReceivers();
-
-	IpfixPrinter* ipfixPrinter = createIpfixPrinter();
-	startIpfixPrinter(ipfixPrinter);
-
-	IpfixReceiver* ipfixReceiver = createIpfixReceiver(lport);
-	addIpfixReceiverCallbacks(ipfixReceiver, getIpfixPrinterCallbackInfo(ipfixPrinter));
-	startIpfixReceiver(ipfixReceiver);
-
-	debugf("Listening on Port %d. Hit Ctrl+C to quit", lport);
-	pause();
-	debug("Stopping threads and tidying up.");
-	
-	stopIpfixReceiver(ipfixReceiver);
-	destroyIpfixReceiver(ipfixReceiver);
- 	deinitializeIpfixReceivers();	
-
-	stopIpfixPrinter(ipfixPrinter);
-	destroyIpfixPrinter(ipfixPrinter);
-	deinitializeIpfixPrinters();
-	*/
-        }
-
-void start_collector_using_new_interface(int lport) {
 	initializeIpfixPrinters();
 	initializeIpfixCollectors();
 
@@ -116,8 +56,10 @@ void start_collector_using_new_interface(int lport) {
 	stopIpfixPrinter(ipfixPrinter);
 	destroyIpfixPrinter(ipfixPrinter);
         deinitializeIpfixPrinters();
-        }
+
+	return 0;
+}
 
 void usage(char* progname) {
-	printf("Usage: %s [-p port] [-n] [-o]\n", progname);
-        }
+	printf("Usage: %s [port] \n", progname);
+}
