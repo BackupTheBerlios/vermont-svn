b7853455df0a91101e0e913c7893aa17d2c7b5b0
sommer <sommer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-04-27 07:07:15 +0000

    Copied Vermont release 0.9 to /releases/vermont-0.9

---
+++ releases/vermont-0.9/sampler/Thread.h
@@ -0,0 +1,60 @@
+/*
+ * PSAMP Reference Implementation
+ *
+ * Thread.h
+ *
+ * Implementation of a base class for a thread
+ *
+ * Author: Michael Drueing <michael@drueing.de>
+ *
+ */
+
+#ifndef THREAD_H
+#define THREAD_H
+
+#include "msg.h"
+
+#include <pthread.h>
+
+typedef void* (*ThreadFunc)(void *);
+
+class Thread
+{
+public:
+	Thread(ThreadFunc threadFunction) : exitFlag(false), f(threadFunction)
+	{
+        };
+
+bool run(void *threadData)
+{
+	//data = threadData;
+	return(pthread_create(&thread, NULL, f, threadData) == 0);
+};
+
+void *join()
+{
+	void *result=NULL;
+        if(!thread || pthread_join(thread, &result)) {
+                msg(MSG_ERROR, "Thread: joining failed");
+        }
+        return result;
+};
+
+bool detach()
+{
+	return (pthread_detach(thread) == 0);
+}
+
+~Thread()
+{
+};
+
+bool exitFlag;
+//void *data;
+
+private:
+	pthread_t thread;
+	ThreadFunc f;
+};
+
+#endif
