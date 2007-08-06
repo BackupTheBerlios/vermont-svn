48cb7a17463a4f554fd5ac5eb011f21373123845
sommer <sommer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-08-06 07:17:19 +0000

    Adapted test_build.sh script

--- branches/vermont/cmake/test_build.sh
+++ branches/vermont/cmake/test_build.sh
@@ -1,6 +1,9 @@
-(./configure --enable-debug --enable-db --enable-pcapmmap&& make clean && make -j 16) || exit 1
-(./configure --enable-debug --enable-db && make clean && make -j 16) || exit 1
-(./configure --enable-db && make clean && make -j 16) || exit 1
-(./configure --enable-debug && make clean && make -j 16) || exit 1
-(./configure --enable-pcapmmap&& make clean && make -j 16) || exit 1
-(./configure && make clean && make -j 16) || exit 1
+#!/bin/sh
+
+(mkdir -p build000 && cd build000 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
+(mkdir -p build001 && cd build001 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=yes .. && make clean && make -j 16 && cd ..) || exit 1
+(mkdir -p build010 && cd build010 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
+(mkdir -p build100 && cd build100 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
+(mkdir -p build110 && cd build110 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
+(mkdir -p build111 && cd build111 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=yes .. && make clean && make -j 16 && cd ..) || exit 1
+
