b6f23f2f94fbcf7ae19389dbdc710289ab682eb9
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-08-29 12:11:32 +0000

    Combination of several merges (already present in SVN tree): make gcc 4.2 happy Bugfix: IpfixDbWriter failed with an exception when receiving ipfix flows Bugfix: previously mentioned bug in IpfixDbWriter was not fully resolved destruction notifications about flow-related elements are not to be handled every time any more corrected definition of some variables for express aggregator cosmetic changes in Hashtable.cpp changed express aggregator inside Hashtable so that raw packets are *not* touched, even when source and destination IPs are to be aggregated with a mask Added cmake CMake build scripts Adapted test_build.sh script Build script fixes for odd library paths and for BSD

--- branches/vermont/portscan/test_build.sh
+++ branches/vermont/portscan/test_build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-(mkdir -p build000 && cd build000 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
-(mkdir -p build001 && cd build001 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=yes .. && make clean && make -j 16 && cd ..) || exit 1
-(mkdir -p build010 && cd build010 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
-(mkdir -p build100 && cd build100 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
-(mkdir -p build110 && cd build110 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=no  .. && make clean && make -j 16 && cd ..) || exit 1
-(mkdir -p build111 && cd build111 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=yes .. && make clean && make -j 16 && cd ..) || exit 1
-
+(mkdir -p build000 && cd build000 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=no  .. && make clean && make -j $job_numbers && cd ..) || exit 1
+(mkdir -p build001 && cd build001 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=yes .. && make clean && make -j $job_numbers && cd ..) || exit 1
+(mkdir -p build010 && cd build010 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=no  .. && make clean && make -j $job_numbers && cd ..) || exit 1
+(mkdir -p build100 && cd build100 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=no  .. && make clean && make -j $job_numbers && cd ..) || exit 1
+(mkdir -p build110 && cd build110 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=no  .. && make clean && make -j $job_numbers && cd ..) || exit 1
+(mkdir -p build111 && cd build111 && cmake -DDEBUG=yes -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=yes .. && make clean && make -j $job_numbers && cd ..) || exit 1
