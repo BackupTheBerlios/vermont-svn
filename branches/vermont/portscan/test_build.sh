cbb39cb250aba328f9876e3f4a07a60bead54a5e
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-08-29 12:13:58 +0000

    corrected mistakes during merge with local git repository

--- branches/vermont/portscan/test_build.sh
+++ branches/vermont/portscan/test_build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+job_numbers=2
+if [ "$1" ]
+then
+	job_numbers=$1
+fi
 (mkdir -p build000 && cd build000 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=no  .. && make clean && make -j $job_numbers && cd ..) || exit 1
 (mkdir -p build001 && cd build001 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=no  -DUSE_PCAPMMAP=yes .. && make clean && make -j $job_numbers && cd ..) || exit 1
 (mkdir -p build010 && cd build010 && cmake -DDEBUG=no  -DSUPPORT_MYSQL=yes -DUSE_PCAPMMAP=no  .. && make clean && make -j $job_numbers && cd ..) || exit 1
