01c2bb3d5245f9510fdf8a376db22cb18cd33907
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-08-29 12:13:34 +0000

    idmefexporter now supports configuration options and generation of timestamps and unique identifiers

--- branches/vermont/portscan/idmef/IDMEFExporter.cpp
+++ branches/vermont/portscan/idmef/IDMEFExporter.cpp
@@ -2,8 +2,27 @@
 #include "IDMEFExporter.h"
 
 #include <sys/stat.h>
+#include <sys/utsname.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 	 
 uint32_t IDMEFExporter::filenameCounter = 0;
+const char* IDMEFExporter::PAR_MESSAGE_ID = "MESSAGE_ID";
+const char* IDMEFExporter::PAR_SOURCE_ADDRESS = "SOURCE_ADDRESS";
+const char* IDMEFExporter::PAR_TARGET_ADDRESS = "TARGET_ADDRESS";
+const char* IDMEFExporter::PAR_ANALYZER_ID = "ANALYZER_ID";
+const char* IDMEFExporter::PAR_ANALYZER_HOST = "ANALYZER_HOST";
+const char* IDMEFExporter::PAR_ANALYZER_IP = "ANALYZER_IP";
+const char* IDMEFExporter::PAR_CREATE_TIME = "CREATE_TIME";
+const char* IDMEFExporter::PAR_NTP_TIME = "NTP_TIME";
+bool IDMEFExporter::filewarningIssued = false;
+Mutex IDMEFExporter::mutex;
+uint32_t IDMEFExporter::idCounter = 0;
+time_t IDMEFExporter::idCounterTime = time(0);
+
 
 /**
  * initializes a new instance of IDMEFExporter
@@ -28,6 +47,27 @@ IDMEFExporter::IDMEFExporter(const string tmplfilename, const string destdir, co
 	if (!S_ISDIR(s.st_mode)) {
 		THROWEXCEPTION("given idmef destination directory %s is not a directory", destdir.c_str());
 	}
+
+	// retrieve default values
+	struct utsname un;
+	if (uname(&un) != 0) THROWEXCEPTION("failed to call uname, error: %s", strerror(errno));
+	char hostname[512];
+	if (gethostname(hostname, 512) != 0) THROWEXCEPTION("failed to call gethostname, error: %s", strerror(errno));
+	struct hostent* he = gethostbyname(hostname);
+	if (he == NULL) THROWEXCEPTION("failed to call gethostbyname, error: %s", strerror(errno));
+
+	this->hostname = string(hostname) + "." + string(un.domainname);
+	ipaddress = inet_ntoa(*((struct in_addr *)he->h_addr));
+	msg(MSG_DIALOG, "using hostname %s and ip address %s", this->hostname.c_str(), ipaddress.c_str());
+
+	// FIXME: let analyzerId be set by configuration
+	analyzerId = "11111";
+
+	srand(time(0));
+}
+
+IDMEFExporter::~IDMEFExporter()
+{
 }
 
 /**
@@ -42,7 +82,7 @@ void IDMEFExporter::readTemplate()
 	char temp[1025];
 	templateContent = "";
 	uint32_t bytes;
-	while ((bytes = fread(temp, 1024, 1, f)) > 0) {
+	while ((bytes = fread(temp, 1, 1024, f)) > 0) {
 		temp[bytes] = 0;
 		templateContent += temp;
 	}
@@ -60,6 +100,16 @@ void IDMEFExporter::setVariable(const string key, const string value)
 }
 
 /**
+ * for documentation see setVariable(string, string)
+ */
+void IDMEFExporter::setVariable(const string key, const uint32_t value)
+{
+	char valtext[15];
+	snprintf(valtext, 15, "%u", value);
+	parameterValues[key] = valtext;
+}
+
+/**
  * get a filename in the destination directory which is not used
  * filenames are started with 00000000, then counting upwards until the 32 bit value is wrapped around
  * we issue a warning, if files are already present in the directory
@@ -71,7 +121,7 @@ string IDMEFExporter::getFilename()
 	snprintf(number, 100, "%010d", filenameCounter);
 	string filename = destinationDir + "/" + number + ".xml";
 	uint32_t counter = 0;
-	while (stat(filename.c_str(), &s) != 0) {
+	while (stat(filename.c_str(), &s) == 0) {
 		if (!filewarningIssued) {
 			msg(MSG_ERROR, "files in IDMEF destination directory are already present, either two processes are writing there simultaneously (VERY BAD, may result in lost or corrupt events), or files from previous run have not been processed yet (ALSO BAD)");
 			filewarningIssued = true;
@@ -85,7 +135,59 @@ string IDMEFExporter::getFilename()
 		filename = destinationDir + "/" + number + ".xml";
 	}
 
+	filenameCounter++;
+
 	return filename;
+}
+
+/**
+ * creates a unique id for the IDMEF message
+ * this function takes input from the current time, the processid, a random variable
+ * and an internal counter
+ */
+string IDMEFExporter::createMessageID()
+{
+	char id[64];
+
+	uint32_t randvar = rand()%10000000;
+	if (idCounter > 0x1000000) {
+		// idCounterTime contains time of last reset of idCounter
+		if (idCounterTime == time(0)) {
+			// this ensures, that all created ids are unique, but is pretty crude
+			// usually we don't send more than 0x1000000 idmef messages per second
+			sleep(1);
+		}
+		idCounterTime = time(0);
+		idCounter = 0;
+	}
+	snprintf(id, 64, "%012X%X%08X%06X", static_cast<uint32_t>(time(0)), randvar, getpid(), idCounter++);
+
+	return id;
+}
+
+/**
+ * return time string for idmef message
+ */
+string IDMEFExporter::getCreateTime(time_t t)
+{
+	char timestr[100];
+	struct tm* tms;
+	tms = localtime(&t);
+	strftime(timestr, sizeof(timestr), "%F-T%TZ", tms);
+	return timestr;
+}
+
+/**
+ * returns ntp time string for idmef message
+ */
+string IDMEFExporter::getNtpStamp(time_t t)
+{
+	char timestr[100];
+
+	// 2208988800 is the amount of seconds between 1900-01-01 and 1970-01-01
+	snprintf(timestr, sizeof(timestr), "0x%lX.0x0", t+2208988800UL); 
+
+	return timestr;
 }
 
 /**
@@ -97,7 +199,21 @@ string IDMEFExporter::getFilename()
  */
 void IDMEFExporter::exportMessage()
 {
+	DPRINTF("sending IDMEF message");
 	string idmefmsg = templateContent;
+
+	// ensure that no idmef messages are simultaneously written by several threads!
+	mutex.lock();
+
+	time_t t = time(0);
+
+	// set idmef parameters
+	setVariable(PAR_ANALYZER_ID, analyzerId);
+	setVariable(PAR_ANALYZER_IP, ipaddress);
+	setVariable(PAR_ANALYZER_HOST, hostname);
+	setVariable(PAR_CREATE_TIME, getCreateTime(t));
+	setVariable(PAR_NTP_TIME, getNtpStamp(t));
+	setVariable(PAR_MESSAGE_ID, createMessageID());
 
 	// replace all variable values inside the template
 	map<string, string>::iterator iter = parameterValues.begin();
@@ -113,22 +229,24 @@ void IDMEFExporter::exportMessage()
 		iter++;
 	}
 
-	// ensure that no idmef messages are simultaneously written by several threads!
-	mutex.lock();
-
 	string filename = getFilename();
 
 	// save message to destination directory
-	FILE* f = fopen(filename.c_str(), "w");
+	FILE* f = fopen(filename.c_str(), "w+");
+	flockfile(f);
+	if (f == NULL) goto error;
 	// first line is URL where processing script should send event to
-	if (fwrite(sendURL.c_str(), sendURL.size(), 1, f) != sendURL.size()) goto error;
+	if (fwrite(sendURL.c_str(), 1, sendURL.size(), f) != sendURL.size()) goto error;
 	if (fwrite("\n", 1, 1, f) != 1) goto error;
-	if (fwrite(idmefmsg.c_str(), idmefmsg.size(), 1, f) != idmefmsg.size()) goto error;
+	if (fwrite(idmefmsg.c_str(), 1, idmefmsg.size(), f) != idmefmsg.size()) goto error;
 	if (fwrite("\n", 1, 1, f) != 1) goto error;
+	funlockfile(f);
 	if (fclose(f) != 0) goto error;
+
+	mutex.unlock();
 
 	return;
 
 error:
-	THROWEXCEPTION("failed to write to file %s, error %s", filename.c_str(), strerror(errno));
+	THROWEXCEPTION("failed to write to file %s, error: %s", filename.c_str(), strerror(errno));
 }
--- branches/vermont/portscan/idmef/IDMEFExporter.h
+++ branches/vermont/portscan/idmef/IDMEFExporter.h
@@ -17,24 +17,46 @@ using namespace std;
 class IDMEFExporter
 {
 	public:
+		// default variable keys (should be set using setVariable)
+		const static char* PAR_SOURCE_ADDRESS; // = "SOURCE_ADDRESS";
+		const static char* PAR_TARGET_ADDRESS; // = "TARGET_ADDRESS";
+
 		IDMEFExporter(const string tmplfilename, const string destdir, const string sendurl);
 		virtual ~IDMEFExporter();
 
 		void setVariable(const string key, const string value);
+		void setVariable(const string key, const uint32_t value);
 		void exportMessage();
 
 	private:
+		// private variable keys which are set by IDMEFExporter
+		const static char* PAR_ANALYZER_ID; // = "ANALYZER_ID";
+		const static char* PAR_ANALYZER_HOST; // = "ANALYZER_HOST";
+		const static char* PAR_ANALYZER_IP; // = "ANALYZER_IP";
+		const static char* PAR_CREATE_TIME; // = "CREATE_TIME";
+		const static char* PAR_MESSAGE_ID; // = "MESSAGE_ID";
+		const static char* PAR_NTP_TIME; // = "NTP_TIME";
+
 		string templateFilename;
 		string destinationDir;
 		string sendURL;
 		string templateContent;
+		string hostname;
+		string ipaddress;
+		string analyzerId;
+
 		map<string, string> parameterValues;
 		static uint32_t filenameCounter;
 		static Mutex mutex;
 		static bool filewarningIssued;
+		static uint32_t idCounter;
+		static time_t idCounterTime;
 
 		void readTemplate();
 		string getFilename();
+		string createMessageID();
+		string getCreateTime(time_t t);
+		string getNtpStamp(time_t t);
 };
 
 #endif
