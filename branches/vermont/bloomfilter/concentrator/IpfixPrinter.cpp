abb3971116f37088c580461761982f7b917a7163
lothar <lothar@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-10-25 14:02:08 +0000

    created branch for bloomfilter implementation

---
+++ branches/vermont/bloomfilter/concentrator/IpfixPrinter.cpp
@@ -0,0 +1,198 @@
+/*
+ * IPFIX Concentrator Module Library
+ * Copyright (C) 2004 Christoph Sommer <http://www.deltadevelopment.de/users/christoph/ipfix/>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "IpfixPrinter.hpp"
+
+
+/**
+ * Creates a new IpfixPrinter. Do not forget to call @c startIpfixPrinter() to begin printing
+ * @return handle to use when calling @c destroyIpfixPrinter()
+ */
+IpfixPrinter::IpfixPrinter() {
+	lastTemplate = 0;	
+	setSinkOwner("IpfixPrinter");
+}
+
+/**
+ * Frees memory used by an IpfixPrinter
+ */
+IpfixPrinter::~IpfixPrinter() {
+}
+
+/**
+ * Starts or resumes printing messages
+ */
+void IpfixPrinter::start() {
+	/* unimplemented, we can't be paused - TODO: or should we? */
+}
+
+/**
+ * Temporarily pauses printing messages
+ */
+void IpfixPrinter::stop() {
+	/* unimplemented, we can't be paused - TODO: or should we? */
+}
+
+/**
+ * Prints a Template
+ * @param sourceID SourceID of the exporting process
+ * @param templateInfo Pointer to a structure defining the Template used
+ */
+int IpfixPrinter::onTemplate(IpfixRecord::SourceID* sourceID, IpfixRecord::TemplateInfo* templateInfo) {
+	printf("\n-+--- Template (id=%u)\n", templateInfo->templateId);
+	printf(" `---\n\n");
+
+	return 0;
+}
+
+/**
+ * Prints a Template that was announced to be destroyed
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ */
+int IpfixPrinter::onTemplateDestruction(IpfixRecord::SourceID* sourceID, IpfixRecord::TemplateInfo* templateInfo) {
+	printf("Destroyed a Template (id=%u)\n", templateInfo->templateId);
+
+	return 0;
+}
+
+/**
+ * Prints a DataRecord
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ * @param length Length of the data block supplied
+ * @param data Pointer to a data block containing all variable fields
+ */
+int IpfixPrinter::onDataRecord(IpfixRecord::SourceID* sourceID, IpfixRecord::TemplateInfo* templateInfo, uint16_t length, IpfixRecord::Data* data) {
+	int i;
+
+	printf("\n-+--- DataRecord (Template id=%u)\n", templateInfo->templateId);
+	printf(" `- variable data\n");
+	for (i = 0; i < templateInfo->fieldCount; i++) {
+		printf(" '   `- ");
+		printFieldData(templateInfo->fieldInfo[i].type, (data + templateInfo->fieldInfo[i].offset));
+		printf("\n");
+	}
+	printf(" `---\n\n");
+
+	return 0;
+}
+
+/**
+ * Prints a OptionsTemplate
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ */
+int IpfixPrinter::onOptionsTemplate(IpfixRecord::SourceID* sourceID, IpfixRecord::OptionsTemplateInfo* optionsTemplateInfo) {
+
+	printf("\n-+--- OptionsTemplate (id=%u)\n", optionsTemplateInfo->templateId);
+	printf(" `---\n\n");
+
+	return 0;
+}
+
+/**
+ * Prints a DataTemplate that was announced to be destroyed
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ */
+int IpfixPrinter::onOptionsTemplateDestruction(IpfixRecord::SourceID* sourceID, IpfixRecord::OptionsTemplateInfo* optionsTemplateInfo) {
+	printf("Destroyed an OptionsTemplate (id=%u)\n", optionsTemplateInfo->templateId);
+
+	return 0;
+}
+
+/**
+ * Prints an OptionsRecord
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ * @param length Length of the data block supplied
+ * @param data Pointer to a data block containing all variable fields
+ */
+int IpfixPrinter::onOptionsRecord(IpfixRecord::SourceID* sourceID, IpfixRecord::OptionsTemplateInfo* optionsTemplateInfo, uint16_t length, IpfixRecord::Data* data) {
+
+	printf("\n-+--- OptionsDataRecord (Template id=%u)\n", optionsTemplateInfo->templateId);
+	printf(" `---\n\n");
+
+	return 0;
+}
+
+/**
+ * Prints a DataTemplate
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ */
+int IpfixPrinter::onDataTemplate(IpfixRecord::SourceID* sourceID, IpfixRecord::DataTemplateInfo* dataTemplateInfo) {
+	int i;
+
+	printf("\n-+--- DataTemplate (id=%u)\n", dataTemplateInfo->templateId);
+	printf(" `- fixed data\n");
+	for (i = 0; i < dataTemplateInfo->dataCount; i++) {
+		printf(" '   `- ");
+		printFieldData(dataTemplateInfo->dataInfo[i].type, (dataTemplateInfo->data + dataTemplateInfo->dataInfo[i].offset));
+		printf("\n");
+	}
+	printf(" `---\n\n");
+
+	return 0;
+}
+
+/**
+ * Prints a DataTemplate that was announced to be destroyed
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ */
+int IpfixPrinter::onDataTemplateDestruction(IpfixRecord::SourceID* sourceID, IpfixRecord::DataTemplateInfo* dataTemplateInfo) {
+	printf("Destroyed a DataTemplate (id=%u)\n", dataTemplateInfo->templateId);
+
+	return 0;
+}
+
+/**
+ * Prints a DataDataRecord
+ * @param sourceID SourceID of the exporting process
+ * @param dataTemplateInfo Pointer to a structure defining the DataTemplate used
+ * @param length Length of the data block supplied
+ * @param data Pointer to a data block containing all variable fields
+ */
+int IpfixPrinter::onDataDataRecord(IpfixRecord::SourceID* sourceID, IpfixRecord::DataTemplateInfo* dataTemplateInfo, uint16_t length, IpfixRecord::Data* data) {
+	int i;
+
+	printf("\n-+--- DataDataRecord (Template id=%u)\n", dataTemplateInfo->templateId);
+	printf(" `- fixed data\n");
+	for (i = 0; i < dataTemplateInfo->dataCount; i++) {
+		printf(" '   `- ");
+		printFieldData(dataTemplateInfo->dataInfo[i].type, (dataTemplateInfo->data + dataTemplateInfo->dataInfo[i].offset));
+		printf("\n");
+	}
+	printf(" `- variable data\n");
+	for (i = 0; i < dataTemplateInfo->fieldCount; i++) {
+		printf(" '   `- ");
+		printFieldData(dataTemplateInfo->fieldInfo[i].type, (data + dataTemplateInfo->fieldInfo[i].offset));
+		printf("\n");
+	}
+	printf(" `---\n\n");
+
+	return 0;
+}
+
---
+++ branches/vermont/bloomfilter/tools/testCollector.cpp
@@ -0,0 +1,97 @@
+/*
+ * IPFIX Concentrator Module Library
+ * Copyright (C) 2004 Christoph Sommer <http://www.deltadevelopment.de/users/christoph/ipfix/>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+/** \file
+ * Separate Program to test the collector
+ * Dumps received flows to stdout
+ */
+
+#include "IpfixCollector.hpp"
+#include "IpfixParser.hpp"
+#include "IpfixPacketProcessor.hpp"
+#include "IpfixReceiverUdpIpV4.hpp"
+#include "IpfixPrinter.hpp"
+
+#include "common/msg.h"
+
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define DEFAULT_LISTEN_PORT 1500
+
+void sigint(int) {
+    static bool shutdownInitiated = false;
+
+    if (shutdownInitiated) {
+        msg(MSG_DIALOG, "second signal received, shutting down the hard way!");
+        exit(2);
+    }
+
+    shutdownInitiated = true;
+}
+
+int main(int argc, char *argv[]) {
+
+	int lport = DEFAULT_LISTEN_PORT;
+
+	msg_setlevel(MSG_DEFAULT);
+
+	signal(SIGINT, sigint);
+
+	if(argv[1]) {
+		lport=atoi(argv[1]);
+	}
+
+	IpfixPrinter ipfixPrinter;
+	ipfixPrinter.start();
+	ipfixPrinter.runSink();
+
+	IpfixReceiverUdpIpV4 ipfixReceiver(lport);
+	/* (not in this branch of rcvIpfix)
+	if (argc > 2) {
+		msg(MSG_DIALOG, "Adding %s to list of authorized hosts", argv[2]);
+		ipfixReceiver.addAuthorizedHost(argv[2]);
+	}
+	*/
+
+	IpfixParser ipfixParser;
+	ipfixParser.addFlowSink(&ipfixPrinter);
+
+	IpfixCollector ipfixCollector;
+	ipfixCollector.addIpfixReceiver(&ipfixReceiver);
+	ipfixCollector.addIpfixPacketProcessor(&ipfixParser);
+	ipfixCollector.start();
+
+	msg(MSG_DIALOG, "Listening on %d. Hit Ctrl+C to quit\n", lport);
+	pause();
+	msg(MSG_DIALOG, "Stopping threads and tidying up.\n");
+
+	msg(MSG_DIALOG, "stopping collector\n");
+	ipfixReceiver.stop();
+
+	msg(MSG_DIALOG, "stopping printer\n");
+	ipfixPrinter.stop();
+
+	return 0;
+}
+
