9e8aa20db03ae6ebe177122a965ecdcde3cf08e4
sistmika <sistmika@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-10-02 11:14:33 +0000

    added missing file (again)

---
+++ branches/vermont/dos-attack/concentrator/Element.h
@@ -0,0 +1,12 @@
+#ifndef ELEMENT_H_
+#define ELEMENT_H_
+template<typename T>
+class Element{
+public:
+	//uint32_t hash;
+	Element* next;
+	Element* prev;
+	T* bucket;
+	Element(T* buck){bucket = buck; prev = 0; next = 0;}
+};
+#endif
