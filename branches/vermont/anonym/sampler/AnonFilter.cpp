c635f03062b4acff6fa2a7869d2898f94300d799
lothar <lothar@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-11-07 11:04:27 +0000

    use anonField in AnonFilter

--- branches/vermont/anonym/sampler/AnonFilter.cpp
+++ branches/vermont/anonym/sampler/AnonFilter.cpp
@@ -3,37 +3,24 @@
 
 #include <anon/AnonModule.h>
 
-#include <ipfixlolib/ipfix_names.h>
-
 bool AnonFilter::processPacket(Packet* p)
 {
 	static uint16_t offset;
 	static unsigned short header;
 	static unsigned long packetClass;
-	static const struct ipfix_identifier* ident;
 
 	for (MethodMap::iterator i = methods.begin(); i != methods.end(); ++i) {
-		if (!Template::getFieldOffsetAndHeader(i->id, &offset, &header, &packetClass)) {
-			msg(MSG_ERROR, "Unkown or unsupported type id %i detected.", i->id);
+		if (!Template::getFieldOffsetAndHeader(i->first, &offset, &header, &packetClass)) {
+			msg(MSG_ERROR, "Unkown or unsupported type id %i detected.", i->first);
 			continue;
 		}
 
-		int len = 0;
-		if (i->len == -1) {
-			if (!(ident = ipfix_id_lookup(i->id))) {
-				msg(MSG_ERROR, "Unknown or unsupported id %i detected.", i->id);
-				continue;
-			}
-			len = ident->length;
-		} else 
-			len = i->len;
-
 		switch (header) {
 		case HEAD_NETWORK:
-			i->method->anonimizeBuffer(p->netHeader + offset, len);
+			anonField(i->first, p->netHeader + offset);
 			break;
 		case HEAD_TRANSPORT:
-			i->method->anonimizeBuffer(p->transportHeader + offset, len);
+			anonField(i->first, p->transportHeader + offset);
 			break;
 		default:
 			msg(MSG_ERROR, "Cannot deal with header type %i", header);
