bddd42d81618b1a9441c242cb9b2b93ded23a506
lothar <lothar@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-11-07 11:04:37 +0000

    added configuration class for RecordAnonymizer

---
+++ branches/vermont/anonym/cfg/RecordAnonymizerCfg.cpp
@@ -0,0 +1,79 @@
+#include "RecordAnonymizerCfg.h"
+#include "InfoElementCfg.h"
+
+RecordAnonymizerCfg* RecordAnonymizerCfg::create(XMLElement* e)
+{
+	assert(e);
+	assert(e->getName() == getName());
+	return new RecordAnonymizerCfg(e);
+}
+
+RecordAnonymizerCfg::RecordAnonymizerCfg(XMLElement* elem)
+	: CfgHelper<RecordAnonymizer, RecordAnonymizerCfg>(elem, "anonRecord")
+{
+}
+
+RecordAnonymizerCfg::~RecordAnonymizerCfg()
+{
+
+}
+
+RecordAnonymizer* RecordAnonymizerCfg::createInstance()
+{
+	if (!instance) {
+		instance = new RecordAnonymizer();
+	}
+
+	XMLNode::XMLSet<XMLElement*> set = _elem->getElementChildren();
+	for (XMLNode::XMLSet<XMLElement*>::iterator it = set.begin();
+	     it != set.end();
+	     it++) {
+		XMLElement* e = *it;
+
+		if (e->matches("anonField")) {
+			InfoElementCfg* cfg = NULL;
+			std::string method;
+			std::string method_parameter;
+			XMLNode::XMLSet<XMLElement*> set = e->getElementChildren();
+			for (XMLNode::XMLSet<XMLElement*>::iterator jt = set.begin();
+			     jt != set.end();
+			     ++jt) {
+				XMLElement* e = *jt;
+				if (e->matches("anonIE")) {
+					if(cfg) {
+						THROWEXCEPTION("Only on anonIE tag per anonField tag allowed");
+					}
+					cfg = new InfoElementCfg(*jt);
+				} else if (e->matches("anonMethod")) {
+					method = get("anonMethod", e);
+				} else if (e->matches("anonParam")) {
+					method_parameter = get("anonParam", e);
+				} else {
+					msg(MSG_ERROR, "Unknown field in anonField");
+					continue;
+				}
+			}
+			if (!cfg) {
+				msg(MSG_FATAL, "Missing information element in anonField");
+				THROWEXCEPTION("Missing information element in anonField");
+			}
+			if (method.empty()) {
+				msg(MSG_FATAL, "Missing anonymization method in anonField");
+				THROWEXCEPTION("Missing anonymization method in anonField");
+			}
+			instance->addAnonymization(cfg->getIeId(), cfg->getIeLength(), AnonMethod::stringToMethod(method), method_parameter);
+			delete cfg;
+				
+		} else {
+			msg(MSG_FATAL, "Unkown anonymization field %s\n", e->getName().c_str());
+			continue;
+		}
+	}
+
+	return instance;
+}
+
+bool RecordAnonymizerCfg::deriveFrom(RecordAnonymizerCfg* old)
+{
+	return true;
+}
---
+++ branches/vermont/anonym/cfg/RecordAnonymizerCfg.h
@@ -0,0 +1,26 @@
+#ifndef _RECORD_ANONYMIZER_CFG_H_
+#define _RECORD_ANONYMIZER_CFG_H_
+
+#include <cfg/Cfg.h>
+
+#include <reconf/RecordAnonymizer.h>
+
+class RecordAnonymizerCfg
+	: public CfgHelper<RecordAnonymizer, RecordAnonymizerCfg>
+{
+public:
+	friend class ConfigManager;
+
+	virtual RecordAnonymizerCfg* create(XMLElement* e);
+	
+	virtual ~RecordAnonymizerCfg();
+
+	virtual RecordAnonymizer* createInstance();
+
+	virtual bool deriveFrom(RecordAnonymizerCfg* old);
+	
+protected:
+	RecordAnonymizerCfg(XMLElement*);
+};
+
+#endif
