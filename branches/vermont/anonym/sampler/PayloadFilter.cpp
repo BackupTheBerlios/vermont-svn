bbcc39b18e089d0fd72a87f7b8a8103f47c5e7e0
lothar <lothar@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-11-07 11:04:17 +0000

    - adopt packet classification after droping payload

--- branches/vermont/anonym/sampler/PayloadFilter.cpp
+++ branches/vermont/anonym/sampler/PayloadFilter.cpp
@@ -3,9 +3,10 @@
 
 bool PayloadFilter::processPacket(Packet* p)
 {
-	if (p->payload) {
+	if (p->classification & PCLASS_PAYLOAD) {
 		// "drop" payload
 		p->data_length = p->payload - p->data;
+		p->classification ^= PCLASS_PAYLOAD;
 	}
 	return true;
 }
