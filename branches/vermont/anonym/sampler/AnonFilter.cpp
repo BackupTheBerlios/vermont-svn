bd619a9e56d2fd324793667ec860806e9823f54d
lothar <lothar@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-11-07 11:04:21 +0000

    move parts of AnonFilter into new AnonModule class

--- branches/vermont/anonym/sampler/AnonFilter.cpp
+++ branches/vermont/anonym/sampler/AnonFilter.cpp
@@ -1,84 +1,9 @@
 #include "AnonFilter.h"
 #include "Template.h"
 
-#include <anon/AnonHashSha1.h>
-#include <anon/AnonBytewiseHashHmacSha1.h>
-#include <anon/AnonBytewiseHashSha1.h>
-#include <anon/AnonConstOverwrite.h>
-#include <anon/AnonContinuousChar.h>
-#include <anon/AnonWhitenoise.h>
-#include <anon/AnonHashHmacSha1.h>
-#include <anon/AnonRandomize.h>
-#include <anon/AnonShuffle.h>
-#include <anon/AnonCryptoPan.h>
+#include <anon/AnonModule.h>
 
 #include <ipfixlolib/ipfix_names.h>
-
-AnonFilter::AnonFilter()
-{
-}
-
-AnonFilter::~AnonFilter()
-{
-	for (MethodMap::iterator i = methods.begin(); i != methods.end(); ++i) {
-		delete i->method;
-	}
-}
-
-AnonPrimitive* AnonFilter::createPrimitive(AnonMethod::Method m, const std::string& parameter)
-{
-	AnonPrimitive* ret = 0;
-	switch (m) {
-	case AnonMethod::HashSha1:
-		ret = new AnonHashSha1();
-		break;
-	case AnonMethod::BytewiseHashSha1:
-		ret = new AnonBytewiseHashSha1();
-		break;
-	case AnonMethod::ContinuousChar:
-		ret = new AnonContinuousChar();
-		break;
-	case AnonMethod::Randomize:
-		ret = new AnonRandomize();
-		break;
-	case AnonMethod::Shuffle:
-		ret = new AnonShuffle();
-		break;
-	case AnonMethod::Whitenoise:
-		ret = new AnonWhitenoise(atoi(parameter.c_str()));
-		break;
-	case AnonMethod::HashHmacSha1:
-		ret = new AnonHashHmacSha1(parameter);
-		break;
-	case AnonMethod::BytewiseHashHmacSha1:
-		ret = new AnonBytewiseHashHmacSha1(parameter);
-		break;
-	case AnonMethod::ConstOverwrite:
-		if (parameter.size() != 1) {
-			THROWEXCEPTION("AnonConstOverwrite only uses one character as key");
-		}
-		ret = new AnonConstOverwrite(parameter.c_str()[0]);
-		break;
-	case AnonMethod::CryptoPan:
-		ret = new AnonCryptoPan(parameter);
-		break;
-	default:
-		msg(MSG_FATAL, "AnonPrimitive number %i is unknown", m);
-		THROWEXCEPTION("AnonPrimitive number %i is unknown", m);
-	}
-
-	return ret;
-}
-
-void AnonFilter::addAnonymization(uint16_t id, int len,  AnonMethod::Method  methodName, const std::string& parameter)
-{
-	AnonPrimitive* a = createPrimitive(methodName, parameter);
-	AnonIE ie;
-	ie.id = id;
-	ie.len = len;
-	ie.method = a;
-	methods.push_back(ie);
-}
 
 bool AnonFilter::processPacket(Packet* p)
 {
--- branches/vermont/anonym/sampler/AnonFilter.h
+++ branches/vermont/anonym/sampler/AnonFilter.h
@@ -2,80 +2,11 @@
 #define _ANON_FILTER_H_
 
 #include "PacketProcessor.h"
+#include <anon/AnonModule.h>
 
-#include <common/CountBloomFilter.h>
-#include <common/AgeBloomFilter.h>
-
-#include <anon/AnonPrimitive.h>
-
-#include <list>
-
-class AnonMethod 
-{
+class AnonFilter : public PacketProcessor, public AnonModule {
 public:
-	typedef enum {
-		BytewiseHashHmacSha1,
-		BytewiseHashSha1,
-		ConstOverwrite,
-		ContinuousChar,
-		HashHmacSha1,
-		HashSha1,
-		Randomize,
-		Shuffle,
-		Whitenoise,
-		CryptoPan
-	} Method;
-
-	static Method stringToMethod(const std::string& m)
-	{
-		if (m == "BytewiseHashHmacSha1") {
-			return BytewiseHashHmacSha1;
-		} else if (m == "BytewiseHashSha1") {
-                        return BytewiseHashSha1;
-                } else if (m == "ConstOverwrite") {
-                        return ConstOverwrite;
-                } else if (m == "ContinuousChar") {
-                        return ContinuousChar;
-                }else if (m == "HashHmacSha1") {
-                        return HashHmacSha1;
-                }else if (m == "HashSha1") {
-                        return HashSha1;
-                }else if (m == "Randomize") {
-                        return Randomize;
-                }else if (m == "Shuffle") {
-                        return Shuffle;
-                }else if (m == "Whitenoise") {
-                        return Whitenoise;
-                }else if (m == "CryptoPan") {
-			return CryptoPan;
-		}
-		THROWEXCEPTION("Unknown anonymization method");
-
-		// make compile happy
-		return BytewiseHashHmacSha1;
-	}
-};
-
-struct AnonIE {
-	uint16_t id;
-	int len;
-	AnonPrimitive* method;
-};
-
-class AnonFilter : public PacketProcessor {
-public:
-	AnonFilter();
-	~AnonFilter();
-
-	void addAnonymization(uint16_t id, int len, AnonMethod::Method m, const std::string& parameter = "");
-
 	virtual bool processPacket(Packet* p);
-protected:
-	typedef std::vector<AnonIE> MethodMap;
-	MethodMap methods;
-
-private:
-	AnonPrimitive* createPrimitive(AnonMethod::Method m, const std::string& paramter);
 };
 
 #endif
