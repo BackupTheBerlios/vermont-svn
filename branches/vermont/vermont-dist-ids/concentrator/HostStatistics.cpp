7a40b12f152edbb118232570cde53c84ab665d21
simasegs <simasegs@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2009-05-08 14:16:55 +0000

    HostStatistics done.

--- branches/vermont/vermont-dist-ids/concentrator/HostStatistics.cpp
+++ branches/vermont/vermont-dist-ids/concentrator/HostStatistics.cpp
@@ -8,6 +8,9 @@
  */
 #include "HostStatistics.h"
 #include "Connection.h"
+#include <netdb.h>
+#include "common/Misc.h"
+
 
 HostStatistics::HostStatistics(std::string ipSubnet, std::string addrFilter, std::string logPath, uint16_t logInt)
 	: ipSubnet(ipSubnet), addrFilter(addrFilter), logPath(logPath), logInt(logInt)
@@ -15,23 +18,16 @@ HostStatistics::HostStatistics(std::string ipSubnet, std::string addrFilter, std
 	// check if srcIP or dstIP in the subnet (1.1.1.1/16)
 	// split string at the '/'
 	size_t found = ipSubnet.find_first_of("/");
-	// convert string IP to uint32_t
 	std::string ip_str = ipSubnet.substr(0, found);
-	size_t pos_1 = 0;
-	size_t pos_2 = 0;
-	for (int i = 0; i < 4; i++) {
-		// shift ip_addr left 8 times (no effect in 1st round)
-		// seperate ip_str at the dots, convert every number-part to integer
-		// "save" the number in ip_addr
-		pos_2 = ip_str.find(".", pos_1);
-		netAddr = (netAddr << 8) | atoi((ip_str.substr(pos_1, pos_2)).c_str());
-		pos_1 = pos_2;
-	}
-	netSize = atoi(ipSubnet.substr(found).c_str());
-	netAddr = htonl(netAddr);
-	// shift the netAddr to the left; this allows '==' comparisons if the other IPs are equally shifted
-	netAddr = (netAddr << netSize);
+	netSize = atoi(ipSubnet.substr(found + 1).c_str());
+	netAddr = *(uint32_t *)gethostbyname(ip_str.c_str())->h_addr;
 	logTimer = time(NULL);
+	
+	// DEBUG
+	FILE* logFile = fopen("host_stats.log", "a");
+	fprintf(logFile, "netAddr: %u\t", netAddr);
+	fprintf(logFile, " - %s\n", IPToString(netAddr).c_str());
+	fclose(logFile);
 }
 
 void HostStatistics::onDataDataRecord(IpfixDataDataRecord* record)
@@ -39,41 +35,46 @@ void HostStatistics::onDataDataRecord(IpfixDataDataRecord* record)
 	Connection conn(record);
 	std::map<uint32_t, uint64_t>::iterator it;
 
-	if ((addrFilter == "src") && ((conn.srcIP << netSize) == netAddr)) {
+	FILE* logFile = fopen("host_stats.log", "a");
+
+	if ((addrFilter == "src") && ((conn.srcIP&netAddr) == netAddr)) {
 		it = trafficMap.find(conn.srcIP);
 		if (it == trafficMap.end())	{
-			trafficMap.insert(pair<uint32_t, uint64_t>(conn.srcIP, conn.srcOctets));
+			trafficMap.insert(pair<uint32_t, uint64_t>(conn.srcIP, ntohl(conn.srcOctets)));
 		} else {
-			//trafficMap[it] += conn.srcOctets;
-			it->second += conn.srcOctets;
+			it->second += ntohl(conn.srcOctets);
 		}
-	} else if ((addrFilter == "dst") && ((conn.dstIP << netSize) == netAddr)) {
+	} else if ((addrFilter == "dst") && ((conn.dstIP&netAddr) == netAddr)) {
 		it = trafficMap.find(conn.dstIP);
 		if (it == trafficMap.end()) {
-			trafficMap.insert(pair<uint32_t, uint64_t>(conn.dstIP, conn.dstOctets));
+			trafficMap.insert(pair<uint32_t, uint64_t>(conn.dstIP, ntohl(conn.dstOctets)));
 		} else {
-			//trafficMap[it] += conn->dstcOctets;
-			it->second += conn.dstOctets;
+			it->second += ntohl(conn.dstOctets);
 		}
 	} else {
-		if ((conn.srcIP << netSize) == netAddr) {
+		if ((conn.srcIP&netAddr) == netAddr) {
+			fprintf(logFile, "Treffer - src\t");
 			it = trafficMap.find(conn.srcIP);
 			if (it == trafficMap.end())	{
-				trafficMap.insert(pair<uint32_t, uint64_t>(conn.srcIP, (conn.srcOctets + conn.dstOctets)));
+				fprintf(logFile, "- %s\n", IPToString(conn.srcIP).c_str());
+				trafficMap.insert(pair<uint32_t, uint64_t>(conn.srcIP, (ntohl(conn.srcOctets) + ntohl(conn.dstOctets))));
 			} else {
-				//trafficMap[it] += (conn.srcOctets + conn.dstOctets);
-				it->second += (conn.srcOctets + conn.dstOctets);
+				fprintf(logFile, "\n");
+				it->second += (ntohl(conn.srcOctets) + ntohl(conn.dstOctets));
 			}
-		} else if ((conn.dstIP << netSize) == netAddr) {
+		} else if ((conn.dstIP&netAddr) == netAddr) {
+			fprintf(logFile, "Treffer - dst\t");
 			it = trafficMap.find(conn.dstIP);
 			if (it == trafficMap.end())	{
-				trafficMap.insert(pair<uint32_t, uint64_t>(conn.dstIP, (conn.srcOctets + conn.dstOctets)));
+				fprintf(logFile, "- %s\n", IPToString(conn.dstIP).c_str());
+				trafficMap.insert(pair<uint32_t, uint64_t>(conn.dstIP, (ntohl(conn.srcOctets) + ntohl(conn.dstOctets))));
 			} else {
-				//trafficMap[it] += (conn.srcOctets + conn.dstOctets);
-				it->second += (conn.srcOctets + conn.dstOctets);
+				fprintf(logFile, "\n");
+				it->second += (ntohl(conn.srcOctets) + ntohl(conn.dstOctets));
 			}
 		}
 	}
+	fclose(logFile);
 }
 
 void HostStatistics::onReconfiguration1()
@@ -82,10 +83,10 @@ void HostStatistics::onReconfiguration1()
 
 	FILE* logFile = fopen(logPath.c_str(), "w");
 	// insert current timestamp
-	fprintf(logFile, "%u", (int)time(NULL));
+	fprintf(logFile, "%d", (int)time(NULL));
 	// for each element in ipList, write an entry like: IP:Bytesum
 	for (it = trafficMap.begin(); it != trafficMap.end(); it++) {
-		fprintf(logFile, " %u:%llu", it->first, it->second);
+		fprintf(logFile, " %s:%u", IPToString(it->first).c_str(), (uint32_t)it->second);
 	}
 	fclose(logFile);
 }
